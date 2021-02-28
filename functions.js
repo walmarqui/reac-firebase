cf809d739c9e60a32eb29ce80d2f8dec1716fddc
walmarqui <walmarqui@gmail.com>
2021-02-27 22:07:41 -0600

    agregando funciones dentro de funciones

--- functions.js
+++ functions.js
@@ -15,3 +15,16 @@ const saluda = (a,b,c) => {
     return a + ' ' + b + ' ' + c
 }
 console.log(saluda('Walter Marinez', 35, true))
+
+
+//funciones dentro de funciones
+const funcion1 = (dato) => {
+    return dato
+}
+
+const funcion2 = (d) => {
+    console.log(d)
+}
+
+funcion2(funcion1('Walter Martinez'))
+
