926fc18567678d4147d00959b924426df93a6f7e
walmarqui <walmarqui@gmail.com>
2021-03-03 22:17:13 -0600

    agregando templates

---
+++ templates.js
@@ -0,0 +1,6 @@
+const nombre = 'Walter'
+const apellido = 'Martinez'
+
+//console.log('Mi nombre es ' + nombre + ' y mi apellido es ' + apellido)
+
+console.log(`mi nombre es ${nombre} y mi apellido es ${apellido}`)
