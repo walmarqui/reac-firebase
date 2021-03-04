a01606f5147959061c689c9e968be40486ae0503
walmarqui <walmarqui@gmail.com>
2021-03-03 22:30:17 -0600

    estructura de un objeto

---
+++ objeto.js
@@ -0,0 +1,12 @@
+const persona = {
+    nombre:'Walter',
+    edad:37,
+    casado:true,
+    hijo:{
+        nombrehijo:'Jean'
+    }
+}
+persona.id = 1
+console.log(persona.id)
+console.log(persona.nombre)
+console.log(persona.hijo.nombrehijo)
